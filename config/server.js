16626913f02b6adc8f40c1d6b069502804a95406
Arek <arkadiuszp@arcor.de>
2022-06-19 20:55:35 +0200

    settings

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
-  host: env("HOST"),
-  port: env("$PORT"),
+  host: env("HOST", "0.0.0.0"),
+  port: env("PORT", 1337),
   app: {
     keys: env.array("APP_KEYS"),
   },
