3143ea12d196062e0e3fbc068f5d31da66eea067
Arek <arkadiuszp@arcor.de>
2022-06-19 11:11:18 +0200

    changing db ssl settings

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -10,7 +10,9 @@ module.exports = ({ env }) => ({
       database: env("DATABASE_NAME"),
       user: env("DATABASE_USERNAME"),
       password: env("DATABASE_PASSWORD"),
-      ssl: env.bool("DATABASE_SSL"),
+      ssl: {
+        rejectUnauthorized: env.bool("DATABASE_SSL"),
+      },
     },
     debug: false,
   },
